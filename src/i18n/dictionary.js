746d30abf4ed0a1c73dd7e728684459b32cdd720
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2020-03-26 18:20:48 +0100

    Improve dictionary (#3)
    split message

--- src/i18n/dictionary.js
+++ src/i18n/dictionary.js
@@ -1,3 +1,6 @@
+import buttonMessage from './buttonMessage';
+import waitingMessage from './waitingMessage';
+
 const dictionary = {
   survey: {
     fr: 'Questionnaire',
@@ -16,23 +19,8 @@ const dictionary = {
     fr: 'Retour au carnet de tournée',
     en: 'Back to circuit book',
   },
-  closeSurvey: { fr: 'Fermer le questionnaire', en: 'Close questionnaire' },
-  saveAndQuit: { fr: 'Sauvegarder et quitter', en: 'Save and quit' },
-  nextContinue: { fr: 'Continuer', en: 'Continue' },
-  goBackReturn: { fr: 'Retour', en: 'Return' },
-  pleaseWait: { fr: 'Veuillez patienter ...', en: 'Please wait ...' },
-  waitingQuestionnaire: {
-    fr: 'Chargement du questionnaire',
-    en: 'Loading questionnaire',
-  },
-  waitingResources: {
-    fr: 'Chargement des ressources du questionnaire',
-    en: 'Loading the questionnaire resources',
-  },
-  waitingDataSU: {
-    fr: "Chargement des données sur l'unité enquêtée",
-    en: 'Loading data on the survey unit',
-  },
+  ...buttonMessage,
+  ...waitingMessage,
 };
 
 export default dictionary;
---
+++ src/i18n/waitingMessage.js
@@ -0,0 +1,17 @@
+const waitingMessage = {
+  pleaseWait: { fr: 'Veuillez patienter ...', en: 'Please wait ...' },
+  waitingQuestionnaire: {
+    fr: 'Chargement du questionnaire',
+    en: 'Loading questionnaire',
+  },
+  waitingResources: {
+    fr: 'Chargement des ressources du questionnaire',
+    en: 'Loading the questionnaire resources',
+  },
+  waitingDataSU: {
+    fr: "Chargement des données sur l'unité enquêtée",
+    en: 'Loading data on the survey unit',
+  },
+};
+
+export default waitingMessage;
