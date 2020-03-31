39ff32b5dc4637bafaee6df82ca46869f62dfd78
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-03-31 16:28:05 +0200

    Update queen Builder
    - remove subsequence when there is no declaration
    - change label of sequence when there is no declaration

--- src/utils/questionnaire/queen.js
+++ src/utils/questionnaire/queen.js
@@ -1,3 +1,12 @@
+import D from 'i18n';
+
+/**
+ * Function to build Queen questionnaire.
+ * We add attribute to components
+ *   - page : the queen page
+ *   -
+ * @param {*} components
+ */
 export const buildQueenQuestionnaire = components => {
   let seq;
   let idSeq;
@@ -5,7 +14,7 @@ export const buildQueenQuestionnaire = components => {
   let idSubseq;
   return Array.isArray(components)
     ? components.reduce((_, component) => {
-        const { componentType, label, id } = component;
+        const { componentType, label, id, declarations } = component;
         if (componentType && !['Sequence', 'Subsequence'].includes(componentType))
           return [
             ..._,
@@ -23,14 +32,24 @@ export const buildQueenQuestionnaire = components => {
           seq = label;
           subseq = '';
           idSubseq = '';
+          /**
+           * if there is no declarations, we display another label : D.newSequence (cf Dictionary)
+           */
+          const newLabel = !declarations || declarations.length === 0 ? D.newSequence : '';
           return [
             ..._,
-            { ...component, labelNav: label, label: '', sequence: seq, page: _.length + 1 },
+            { ...component, labelNav: label, label: newLabel, sequence: seq, page: _.length + 1 },
           ];
         }
         if (componentType === 'Subsequence') {
           idSubseq = id;
           subseq = label;
+          /**
+           * if there is no declarations, we delete this component
+           */
+          if (!declarations || declarations.length === 0) {
+            return _;
+          }
           return [
             ..._,
             {
