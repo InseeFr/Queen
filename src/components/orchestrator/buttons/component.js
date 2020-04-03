34927efeb822e52fc58b2628a8619b0f34b2acc6
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-04-03 12:23:59 +0200

    Change button lunatic to jsx button

--- src/components/orchestrator/buttons/component.js
+++ src/components/orchestrator/buttons/component.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@inseefr/lunatic';
 import D from 'i18n';
 import styles from './buttons.scss';
 
 const Buttons = ({ nbModules, page, pagePrevious, pageNext, pageFastForward, quit }) => {
-  const btnDown = page === 0 ? '' : D.goBackReturn;
-  const finalPage = nbModules - 1 === page ? D.saveAndQuit : `${D.nextContinue} \u2192`;
+  const returnLabel = page === 0 ? '' : D.goBackReturn;
+  const nextLabel = nbModules - 1 === page ? D.saveAndQuit : `${D.nextContinue} \u2192`;
   const pageNextFunction = nbModules - 1 === page ? quit : pageNext;
 
   return (
     <>
       <style type="text/css">{styles}</style>
-      <div id="buttons" className={`buttons ${!btnDown && 'btn-alone'}`}>
-        {btnDown && <Button value={btnDown} onClick={pagePrevious} />}
-        <Button value={finalPage} onClick={pageNextFunction} />
-        <Button value={`${D.fastForward} \u21E5`} onClick={pageFastForward} />
+      <div id="buttons" className={`buttons ${!returnLabel && 'btn-alone'}`}>
+        {returnLabel && (
+          <button className="navigation-button" type="button" onClick={pagePrevious}>
+            {returnLabel}
+          </button>
+        )}
+        <button className="navigation-button" type="button" onClick={pageNextFunction}>
+          {nextLabel}
+        </button>
+        <button className="navigation-button" type="button" onClick={pageFastForward}>
+          {`${D.fastForward} \u21E5`}
+        </button>
       </div>
     </>
   );
