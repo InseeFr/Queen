6a38c83f3bb24314b06efc09d98e1a60e81b270e
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-04-16 15:57:31 +0200

    Add questionnaire-cache to SW

--- src/external-sw.js
+++ src/external-sw.js
@@ -2,11 +2,18 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
 
-const getUrlRegex = function(url) {
+const getUrlRegex = url => {
   return url
     .replace('http', '^http')
     .replace('.', '\\\\.')
     .concat('/(.*)((.json)|(.js)|(.png)|(.svg))');
+};
+
+const getQuestionnaireUrlRegex = urlQueenApi => {
+  return urlQueenApi
+    .replace('http', '^http')
+    .replace('.', '\\\\.')
+    .concat('/api/operation/(.){1,}/questionnaire');
 };
 
 if (workbox) {
@@ -15,6 +22,25 @@ if (workbox) {
   console.log('Loading Queen SW into another SW');
   const queenPrecacheController = new workbox.precaching.PrecacheController(queenCacheName);
   queenPrecacheController.addToCacheList(self.__precacheManifest);
+
+  const setQuestionnaireCache = async () => {
+    const responseFromQueen = await fetch(`${self._urlQueen}/configuration.json`);
+    const configuration = await responseFromQueen.json();
+
+    workbox.routing.registerRoute(
+      new RegExp(getQuestionnaireUrlRegex(`${configuration.urlQueenApi}`)),
+      new workbox.strategies.CacheFirst({
+        cacheName: 'queen-questionnaire',
+        plugins: [
+          new workbox.cacheableResponse.Plugin({
+            statuses: [0, 200],
+          }),
+        ],
+      })
+    );
+  };
+
+  setQuestionnaireCache();
 
   workbox.routing.registerRoute(
     new RegExp(getUrlRegex(self._urlQueen)),
