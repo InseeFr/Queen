1fc2145f5c65285a2b70e3c748d63f4b629cfd08
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-04-16 15:47:15 +0200

    update init function for orchestrator

--- src/utils/initializeOrchestrator/anonymous.js
+++ src/utils/initializeOrchestrator/anonymous.js
@@ -14,45 +14,45 @@ export const initialize = ({
   setWaitingMessage,
   setQuestionnaire,
   setSurveyUnit,
-}) => {
+}) => async () => {
   const { standalone, urlQueenApi } = configuration;
-  const init = async () => {
-    /**
-     * Get questionnaire
-     *    standalone mode : get from the API (always online)
-     *    embedded mode   : get from the API or service-worker
-     */
-    setWaitingMessage(D.waitingQuestionnaire);
-    const fetchedQuestionnaire = await getQuestionnaireById(urlQueenApi, idQuestionnaire);
-    // set questionnaire to orchestrator
-    setQuestionnaire(fetchedQuestionnaire);
+  /**
+   * Get questionnaire
+   *    standalone mode : get from the API (always online)
+   *    embedded mode   : get from the API or service-worker
+   */
+  setWaitingMessage(D.waitingQuestionnaire);
+  const response = await getQuestionnaireById(urlQueenApi, null)(idQuestionnaire);
+  const questionnaire = await response.data;
+  // set questionnaire to orchestrator
+  setQuestionnaire(questionnaire);
 
-    /**
-     * Get resources for questionnaire
-     * (waiting for spec)
-     */
-    if (standalone) {
-      setWaitingMessage(D.waitingResources);
-      await getResourceById(idQuestionnaire);
-    }
-    /**
-     * Get survey-unit's data
-     *    standalone mode : get data from API, then from database
-     *    embedded mode   : get data from database
-     */
-    setWaitingMessage(D.waitingDataSU);
-    if (standalone) {
-      const fetchedData = await getDataSurveyUnitById(idSurveyUnit);
-      const fetchedComment = await getCommentSurveyUnitById(idSurveyUnit);
-      await surveyUnitIdbService.addOrUpdateSU({
-        idSU: idSurveyUnit,
-        data: fetchedData,
-        comment: fetchedComment,
-      });
-    }
-    const surveyUnit = await surveyUnitIdbService.getByIdSU(idSurveyUnit);
-    // set survey unit data to orchestrator
-    setSurveyUnit(surveyUnit);
-  };
-  return init;
+  /**
+   * Get resources for questionnaire
+   * (waiting for spec)
+   */
+  if (standalone) {
+    setWaitingMessage(D.waitingResources);
+    await getResourceById(idQuestionnaire);
+  }
+  /**
+   * Get survey-unit's data
+   *    standalone mode : get data from API, then from database
+   *    embedded mode   : get data from database
+   */
+  setWaitingMessage(D.waitingDataSU);
+  if (standalone) {
+    const dataResponse = await getDataSurveyUnitById(urlQueenApi, null)(idSurveyUnit);
+    const surveyUnitData = await dataResponse.data;
+    const commentResponse = await getCommentSurveyUnitById(urlQueenApi, null)(idSurveyUnit);
+    const surveyUnitComment = await commentResponse.data;
+    await surveyUnitIdbService.addOrUpdateSU({
+      idSU: idSurveyUnit,
+      data: surveyUnitData,
+      comment: surveyUnitComment,
+    });
+  }
+  const surveyUnit = await surveyUnitIdbService.getByIdSU(idSurveyUnit);
+  // set survey unit data to orchestrator
+  setSurveyUnit(surveyUnit);
 };
