3cee2b8b0148d87ed1eacc1aebc955baaa83fe85
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-05-13 17:30:49 +0200

    Add navigation Message
    - back
    - survey button

--- src/i18n/navigationMessage.js
+++ src/i18n/navigationMessage.js
@@ -1,10 +1,12 @@
 const navigationMessage = {
   newSequenceComment: { fr: 'Changement de séquence', en: 'Changing the sequence' },
   goToNavigation: { fr: 'Allez vers ...', en: 'Go to ...' },
+  goBackNavigation: { fr: 'Retour', en: 'Go back' },
   backToBeginning: {
     fr: 'Retour au début du questionnaire',
     en: 'Back to the beginning of the questionnaire',
   },
+  surveyNavigation: { fr: 'Enquête', en: 'Survey' },
 };
 
 export default navigationMessage;
