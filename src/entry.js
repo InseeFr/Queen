066435736cf4f16601d1b7763c93c2fd567e6c15
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-05-17 19:29:00 +0200

    Remove webpack configuration

--- src/entry.js
+++
@@ -1,2 +0,0 @@
-import './public-url';
-import './index';
--- src/public-url.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-undef */
-const urlQueen = localStorage.getItem('QUEEN_URL');
-__webpack_public_path__ = urlQueen ? `${urlQueen}/` : '/';
