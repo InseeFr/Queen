6193c2a8e1c3d9154c2f7836a20437a173975c2e
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-07-01 11:17:33 +0200

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,6 +14,7 @@ const App = () => {
       {configuration && <Rooter configuration={configuration} />}
       {!configuration && <Preloader message={D.waitingConfiguration} />}
     </root.div>
+    <
   );
 };
 
