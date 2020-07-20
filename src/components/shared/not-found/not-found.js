7bf7d13ac09cf1137c3cfbf251276439d9131ccb
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-07-20 16:30:45 +0200

    Update not-found.js
    delete sync button

--- src/components/shared/not-found/not-found.js
+++ src/components/shared/not-found/not-found.js
@@ -2,14 +2,12 @@ import React from 'react';
 import D from 'i18n';
 import { StyleWrapper } from './not-found.style';
 import { version } from '../../../../package.json';
-import { synchronize } from 'utils/synchronize';
 
 export default () => (
   <StyleWrapper>
     <div className="content">
       <h1>{D.pageNotFound}</h1>
       <h2>{D.pageNotFoundHelp}</h2>
-      <button onClick={synchronize}>synchronize</button>
     </div>
     <div className="version">{`Version ${version}`}</div>
   </StyleWrapper>
