69f33f460b45b47c9f435d44cc69a03d168e5df8
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-07-24 12:01:30 +0200

    Delete synchronize handler event

--- src/utils/communication/eventHandler.js
+++ src/utils/communication/eventHandler.js
@@ -1,24 +1,8 @@
-import { synchronize } from 'utils/synchronize';
-import { sendSynchronizeEvent, sendReadyEvent } from './eventSender';
+import { sendReadyEvent } from './eventSender';
 
 const handleEventParentApp = event => {
   if (event.detail) {
     const { command } = event.detail;
-    if (command === 'SYNCHRONIZE') {
-      const launchSynchronize = async () => {
-        console.log('Queen synchronization : STARTED !');
-        try {
-          await synchronize();
-          sendSynchronizeEvent('SUCCESS');
-          console.log('Queen synchronization : ENDED !');
-        } catch (e) {
-          console.log(e.message);
-          sendSynchronizeEvent('FAILURE');
-          console.log('Queen synchronization : ENDED !');
-        }
-      };
-      launchSynchronize();
-    }
     if (command === 'HEALTH_CHECK') {
       sendReadyEvent();
     }
