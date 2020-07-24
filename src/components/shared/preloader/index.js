53a916757d10ad3f2ca358b649a9a8e0bff97ed3
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-07-24 12:01:59 +0200

    Improver preloader

--- src/components/shared/preloader/index.js
+++ src/components/shared/preloader/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import imgPreloader from 'img/preloader.svg';
 import D from 'i18n';
 import { version } from '../../../../package.json';
 import { StyleWrapper } from './preloader.style';
 
-const Preloader = ({ message = '' }) => (
+const Preloader = ({ title, message }) => (
   <StyleWrapper>
     <div className="preloader">
       <img src={imgPreloader} alt="waiting..." />
-      <h2>{D.pleaseWait}</h2>
+      <h2>{title}</h2>
       <h3>{message}</h3>
     </div>
     <div className="version">{`Version ${version}`}</div>
   </StyleWrapper>
 );
+
+Preloader.defaultProps = {
+  title: D.pleaseWait,
+  message: '',
+};
+
+Preloader.propTypes = {
+  title: PropTypes.string,
+  message: PropTypes.string,
+};
 
 export default Preloader;
