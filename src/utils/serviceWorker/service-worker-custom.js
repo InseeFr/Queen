a9f661e524ebd96e16e02795a10a9901d7d26b8b
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-10-20 17:27:54 +0200

    Update icons

--- src/utils/serviceWorker/service-worker-custom.js
+++ src/utils/serviceWorker/service-worker-custom.js
@@ -1,43 +1,13 @@
 importScripts('/service-worker.js');
 
-const getUrlRegexJson = function(url) {
-  return url.replace('http', '^http').concat('/(.*)(.json)');
+const getUrlRegex = function(url) {
+  return url.replace('http', '^http').concat('/(.*)((.ico)|(.png)|(.json))');
 };
 
-const getUrlRegexManifestFiles = function(url) {
-  return url.replace('http', '^http').concat('/(.*)((.ico)|(.png))');
-};
-
-const configurationCacheName = 'configuration-cache';
-const manifestImageCacheName = 'manifest-cache';
-
-self.addEventListener('install', event => {
-  console.log('Queen  sw : installing configuration..');
-  const cacheName = configurationCacheName;
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      cache.addAll([
-        `${self.location.origin}/configuration.json`,
-        `${self.location.origin}/keycloak.json`,
-        `${self.location.origin}/manifest.json`,
-      ]);
-      return cache;
-    })
-  );
-  event.waitUntil(
-    caches.open(manifestImageCacheName).then(function(cache) {
-      const imageSizes = ['72', '96', '128', '144', '152', '192', '384', '512'];
-      const imageUrl = imageSizes.map(
-        size => `${self.location.origin}/static/images/icons/icon-${size}x${size}.png`
-      );
-      cache.addAll([...imageUrl, `${self.location.origin}/favicon.ico`]);
-      return cache;
-    })
-  );
-});
+const configurationCacheName = 'queen-cache';
 
 workbox.routing.registerRoute(
-  new RegExp(getUrlRegexJson(self.location.origin)),
+  new RegExp(getUrlRegex(self.location.origin)),
   new workbox.strategies.CacheFirst({
     cacheName: configurationCacheName,
     plugins: [
@@ -47,14 +17,25 @@ workbox.routing.registerRoute(
     ],
   })
 );
-workbox.routing.registerRoute(
-  new RegExp(getUrlRegexManifestFiles(self.location.origin)),
-  new workbox.strategies.CacheFirst({
-    cacheName: manifestImageCacheName,
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
+
+const queenPrecacheController = async () => {
+  const responseFromQueen = await fetch('/manifest.json');
+  const { icons } = await responseFromQueen.json();
+  const urlsToPrecache = [
+    `/keycloak.json`,
+    `/manifest.json`,
+    `/configuration.json`,
+    `/static/questionnaire/simpsons.json`,
+  ].concat(icons.map(({ src }) => src));
+  const cache = await caches.open(configurationCacheName);
+  await cache.addAll(urlsToPrecache);
+};
+
+self.addEventListener('install', event => {
+  console.log('QUEEN sw : installing ...');
+  event.waitUntil(queenPrecacheController());
+});
+
+self.addEventListener('activate', event => {
+  console.log('QUEEN sw : activating ...');
+});
