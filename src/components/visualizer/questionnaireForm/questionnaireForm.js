dea980ffa00822a26382da0a9bfd7b115798cd09
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-03-01 16:32:34 +0100

    Refactor & improve style of visualizer

--- src/components/visualizer/questionnaireForm/questionnaireForm.js
+++ src/components/visualizer/questionnaireForm/questionnaireForm.js
@@ -1,44 +1,119 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, AppVersion } from 'components/designSystem';
 import D from 'i18n';
-import { QUESTIONNAIRE_EXAMPLE_URL, SIMPSONS } from 'utils/constants';
+import {
+  DATA_EXAMPLE_URL,
+  QUESTIONNAIRE_EXAMPLE_URL,
+  SIMPSONS,
+  DEFAULT_DATA_URL,
+} from 'utils/constants';
 import { useHistory } from 'react-router-dom';
-import { version } from '../../../../package.json';
+import { Container, makeStyles, Typography, TextField } from '@material-ui/core';
+import Examples from './examples';
+import Helper from './helper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingBottom: theme.spacing(3),
+  },
+  title: {
+    textAlign: 'center',
+  },
+  selectionParent: {
+    display: 'flex',
+    alignItems: 'baseLine',
+  },
+  selection: {
+    marginLeft: theme.spacing(3),
+  },
+  buttonParent: {
+    marginTop: theme.spacing(1),
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  button: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(5),
+  },
+}));
 
 const QuestionnaireForm = () => {
-  const [value, setValue] = useState('');
+  const classes = useStyles();
+  const [questionnaire, setQuestionnaire] = useState('');
+  const [data, setData] = useState('');
+
+  const [selected, setSelected] = useState('');
+
+  useEffect(() => {
+    setQuestionnaire(selected ? QUESTIONNAIRE_EXAMPLE_URL(selected) : selected);
+    setData(selected ? DATA_EXAMPLE_URL(selected) : selected);
+  }, [selected]);
+
   const history = useHistory();
 
   const goToQuestionnaire = e => {
     history.push({
       pathname: '/queen/visualize',
-      search: `?questionnaire=${encodeURIComponent(value || QUESTIONNAIRE_EXAMPLE_URL(SIMPSONS))}`,
+      search: `?questionnaire=${encodeURIComponent(questionnaire)}${
+        data ? `&data=${encodeURIComponent(data)}` : ''
+      }`,
     });
     e.preventDefault();
   };
 
   return (
-    <>
-      <div className="visualize-content">
-        <h1 className="title">{D.visualizationTitlePage}</h1>
-        <form onSubmit={goToQuestionnaire}>
-          <label htmlFor="input-questionnaire" id="label-input-questionnaire">
-            {`${D.questionnaireUrl} :`}
-          </label>
-          <input
-            id="input-questionnaire"
-            className="input-questionnaire"
-            type="input"
-            value={value}
-            placeholder={QUESTIONNAIRE_EXAMPLE_URL(SIMPSONS)}
-            onChange={({ target: { value: v } }) => {
-              setValue(v);
-            }}
-          />
-          <input type="submit" className="button-questionnaire" value={D.visualize} />
-        </form>
-      </div>
-      <div className="version">{`Version ${version}`}</div>
-    </>
+    <Container maxWidth="lg" className={classes.root}>
+      <Typography variant="h3" className={classes.title}>
+        {D.visualizationTitlePage}
+      </Typography>
+      <form onSubmit={goToQuestionnaire}>
+        <TextField
+          id="questionnaire-url-form"
+          required
+          label={D.labelQuest}
+          placeholder={QUESTIONNAIRE_EXAMPLE_URL(SIMPSONS)}
+          helperText={D.helperTextQuest}
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={questionnaire}
+          onChange={({ target: { value: v } }) => {
+            setQuestionnaire(v);
+          }}
+          variant="outlined"
+        />
+        <TextField
+          id="data-url-form"
+          label={D.labelData}
+          placeholder={DEFAULT_DATA_URL}
+          helperText={D.helperTextData}
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={data}
+          onChange={({ target: { value: v } }) => {
+            setData(v);
+          }}
+          variant="outlined"
+        />
+        <div className={classes.selectionParent}>
+          <Typography>{D.chooseExamples}</Typography>
+          <Examples className={classes.selection} selected={selected} setSelected={setSelected} />
+        </div>
+        <div className={classes.buttonParent}>
+          <Button type="submit" className={classes.button}>
+            {D.visualize}
+          </Button>
+        </div>
+      </form>
+      <br />
+      <Helper />
+      <AppVersion />
+    </Container>
   );
 };
 
