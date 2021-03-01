becf7f66ca767f6e933aa184381d1c9ee171e42e
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-03-01 16:34:06 +0100

    Rename root to Router

--- src/components/router/Rooter.js
+++ src/components/router/Rooter.js
@@ -19,7 +19,7 @@ const Rooter = () => {
           path={`/queen/:${READ_ONLY}?/questionnaire/:idQ/survey-unit/:idSU`}
           component={secure(OrchestratorManager)}
         />
-        {!standalone && <Route path="/queen/synchronize" component={secure(Synchronize)} />}
+        {standalone && <Route path="/queen/synchronize" component={secure(Synchronize)} />}
         <Route path="/queen/visualize" component={Visualizer} />
         {!standalone && pathname.startsWith('/queen') && <Redirect to="/queen/visualize" />}
         {standalone && !pathname.startsWith('/authentication') && (
