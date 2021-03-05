cbfd8a03451e4bc6ea87f9e32880610099e05cbe
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-03-05 21:18:22 +0100

    Improve entry.js

--- public/entry.js
+++ public/entry.js
@@ -1,18 +1,24 @@
 const loadQueen = async () => {
   console.log('Loading queen');
-  const queenUrl = new URL(document.currentScript.src).origin;
-  window.localStorage.setItem('QUEEN_URL', queenUrl);
+  try {
+    const queenUrl = new URL(document.currentScript.src).origin;
+    window.localStorage.setItem('QUEEN_URL', queenUrl);
 
-  const manifest = await fetch(`${queenUrl}/asset-manifest.json`);
-  const { entrypoints } = await manifest.json();
+    const manifest = await fetch(`${queenUrl}/asset-manifest.json`);
+    const { entrypoints } = await manifest.json();
 
-  entrypoints.forEach(scriptUrl => {
-    if (scriptUrl.endsWith('.js')) {
-      const script = document.createElement('script');
-      script.src = `${queenUrl}/${scriptUrl}`;
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  });
+    entrypoints.forEach(scriptUrl => {
+      if (scriptUrl.endsWith('.js')) {
+        const script = document.createElement('script');
+        script.src = `${queenUrl}/${scriptUrl}`;
+        script.async = true;
+        document.body.appendChild(script);
+      }
+    });
+    console.log('Queen was successfully loaded');
+  } catch (error) {
+    console.error(error);
+    console.error('Failed to load Queen as Web Component');
+  }
 };
 loadQueen();
