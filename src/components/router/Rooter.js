e122ff1cb7cd59705750243b84de70aae01cd786
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-04-07 12:15:32 +0200

    Create queenRedirect component for Pearl access

--- src/components/router/Rooter.js
+++ src/components/router/Rooter.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import { READ_ONLY } from 'utils/constants';
 import OrchestratorManager from 'components/orchestratorManager';
+import QueenRedirect from 'components/queenRedirect';
 import Synchronize from 'components/Synchronize';
 import Visualizer from 'components/visualizer';
 import { AppContext } from 'components/app';
@@ -17,7 +18,8 @@ const Rooter = () => {
         path={`/queen/:${READ_ONLY}?/questionnaire/:idQ/survey-unit/:idSU`}
         component={secure(OrchestratorManager)}
       />
-      {!standalone && <Route path="/queen/synchronize" component={secure(Synchronize)} />}
+      <Route path={`/queen/:${READ_ONLY}?/survey-unit/:idSU`} component={secure(QueenRedirect)} />
+      {standalone && <Route path="/queen/synchronize" component={secure(Synchronize)} />}
       <Route path="/queen/visualize" component={Visualizer} />
       {!standalone &&
         !pathname.startsWith('/queen/authentication') &&
--- src/i18n/errorMessage.js
+++ src/i18n/errorMessage.js
@@ -1,5 +1,9 @@
 const errorMessage = {
   errorOccurred: { fr: 'Une erreur est survenue', en: 'An error has occurred' },
+  failedToLoadSurveyUnit: {
+    fr: "Impossible de charger l'unité enquêtée",
+    en: 'Unable to load the survey unit',
+  },
   questionnaireNotFound: {
     fr: 'Questionnaire introuvable',
     en: 'Questionnaire not found',
