73ff969bc81747d50e7f2dfcd934ea9be0eaf797
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-04-07 12:15:05 +0200

    Improve right panel (display pages)

--- src/components/orchestrator/rightNavbar/component.js
+++ src/components/orchestrator/rightNavbar/component.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import * as UQ from 'utils/questionnaire';
 import { useStyles } from './component.style';
 
-const NavBar = ({ nbModules, page, children }) => {
+const NavBar = ({ maxPages, page, children, occurences }) => {
   const classes = useStyles();
 
-  const currentPage = page;
-  const nbTotalPage = nbModules;
+  const currentLocalPages = page.includes('.')
+    ? UQ.getPageWithoutAnyIteration(page).split('.')
+    : [page];
 
+  const displayPages = currentLocalPages
+    .map((p, i) => {
+      return { localScopePage: p, localScopeMaxPage: maxPages[i] };
+    })
+    .reverse();
+
+  const occurencesOrdered = occurences.slice().reverse();
   return (
     <div className={classes.root}>
-      <div className={classes.page}>
-        <div className={classes.labelPage}>n° page</div>
-        <div>
-          <b>{`${currentPage}/${nbTotalPage}`}</b>
-        </div>
+      <div className={classes.pages}>
+        {displayPages.map((pages, i) => {
+          const { localScopePage, localScopeMaxPage } = pages;
+          return (
+            <>
+              {occurencesOrdered && occurencesOrdered[i] && (
+                <div className={classes.page}>
+                  <div className={classes.labelPage}>Occurence</div>
+                  {occurencesOrdered[i].map((v, o) => {
+                    return (
+                      <b key={`${v}-${o}`} className={classes.detail}>
+                        {v}
+                      </b>
+                    );
+                  })}
+                </div>
+              )}
+              <div key={`${localScopePage}-${i}`} className={classes.page}>
+                <div className={classes.labelPage}>n° page</div>
+                <div>
+                  <b>{`${localScopePage}/${localScopeMaxPage}`}</b>
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
       {children}
     </div>
   );
-};
-
-NavBar.propTypes = {
-  nbModules: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
 };
 
 export default React.memo(NavBar);
