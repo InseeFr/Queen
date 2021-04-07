cdc96439966ac734db460e2ef1fceb5a6a3cc454
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-04-07 15:20:08 +0200

    Update checkVersion method
    - true if major version is the same and minorVersion >= version

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -48,5 +48,5 @@ export const DATA_EXAMPLE_URL = q =>
 
 export const DEFAULT_DATA_URL = DATA_EXAMPLE_URL(DEFAULT);
 
-export const LUNATIC_MODEL_VERSION = '2.1.0';
-export const ENO_CORE_VERSION = '2.2.5';
+export const MIN_LUNATIC_MODEL_VERSION = '2.1.0';
+export const MIN_ENO_CORE_VERSION = '2.3.0';
--- src/utils/questionnaire/questionnaire.js
+++ src/utils/questionnaire/questionnaire.js
@@ -1,18 +1,33 @@
-import { LUNATIC_MODEL_VERSION, ENO_CORE_VERSION } from 'utils/constants';
+import { MIN_LUNATIC_MODEL_VERSION, MIN_ENO_CORE_VERSION } from 'utils/constants';
+
+const checkVersion = (actualVersion, expectedVersion) => {
+  const [major, minor] = expectedVersion.split('.');
+  const [majorA, minorA] = actualVersion.split('.');
+
+  try {
+    const majorInt = parseInt(major, 10);
+    const minorInt = parseInt(minor, 10);
+    const majorAInt = parseInt(majorA, 10);
+    const minorAInt = parseInt(minorA, 10);
+    if (majorInt === majorAInt && minorAInt >= minorInt) return { valid: true };
+    return { valid: false };
+  } catch (e) {
+    return { valid: false };
+  }
+};
 
 export const checkVersions = ({ enoCoreVersion, lunaticModelVersion }) => {
-  if (
-    !enoCoreVersion.startsWith(ENO_CORE_VERSION) ||
-    !lunaticModelVersion.startsWith(LUNATIC_MODEL_VERSION)
-  ) {
-    const enoMessage = !enoCoreVersion.startsWith(ENO_CORE_VERSION)
-      ? `The Eno version of questionnaire is not compatible (actual : ${enoCoreVersion}, expected : ${ENO_CORE_VERSION}).`
+  const { valid: enoValid } = checkVersion(enoCoreVersion, MIN_ENO_CORE_VERSION);
+  const { valid: lunaticValid } = checkVersion(lunaticModelVersion, MIN_LUNATIC_MODEL_VERSION);
+  if (!enoValid || !lunaticValid) {
+    const enoMessage = !enoValid
+      ? `The Eno version of questionnaire is not compatible (actual : ${enoCoreVersion}, min expected : ${MIN_ENO_CORE_VERSION}).`
       : '';
-    const lunaticMessage = !lunaticModelVersion.startsWith(LUNATIC_MODEL_VERSION)
-      ? `The Lunatic-Model version of questionnaire is not compatible (actual : ${lunaticModelVersion}, expected : ${LUNATIC_MODEL_VERSION}).`
+    const lunaticMessage = !lunaticValid
+      ? `The Lunatic-Model version of questionnaire is not compatible (actual : ${lunaticModelVersion}, min expected : ${MIN_LUNATIC_MODEL_VERSION}).`
       : '';
     return {
-      valid: false,
+      valid: enoValid && lunaticValid,
       error: `${enoMessage} ${lunaticMessage}`,
     };
   }
