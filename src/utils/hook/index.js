caea148530110d7c2272ac02693aac9a5cccf1dd
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-04-13 13:23:07 +0200

    Create hook to manage questionnaire state

--- src/utils/hook/index.js
+++ src/utils/hook/index.js
@@ -22,3 +22,14 @@ export const useAsyncValue = f => {
   }, [f]);
   return refreshFunction;
 };
+
+export const usePrevious = (value, initial) => {
+  const ref = useRef({ target: value, previous: initial });
+
+  if (ref.current.target !== value) {
+    ref.current.previous = ref.current.target;
+    ref.current.target = value;
+  }
+
+  return ref.current.previous;
+};
