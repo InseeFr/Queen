fc7489af6fed2828e9ed65996769c8def4ec9afd
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2021-12-16 16:56:47 +0100

    Update Lunatic (#119)

--- src/components/orchestrator/navigation/component.js
+++ src/components/orchestrator/navigation/component.js
@@ -24,8 +24,9 @@ import { Apps } from '@material-ui/icons';
 import { OrchestratorContext } from '../queen';
 
 const Navigation = ({ className, title }) => {
-  const { questionnaire, bindings, validatedPages, setMenuOpen, readonly, setPage } =
-    useContext(OrchestratorContext);
+  const { questionnaire, bindings, validatedPages, setMenuOpen, readonly, setPage } = useContext(
+    OrchestratorContext
+  );
   const [open, setOpen] = useState(false);
   const [surveyOpen, setSurveyOpen] = useState(false);
   const [stopOpen, setStopOpen] = useState(false);
@@ -125,10 +126,9 @@ const Navigation = ({ className, title }) => {
     )
   );
 
-  const setFocus = useCallback(
-    index => () => setCurrentFocusElementIndex(index),
-    [setCurrentFocusElementIndex]
-  );
+  const setFocus = useCallback(index => () => setCurrentFocusElementIndex(index), [
+    setCurrentFocusElementIndex,
+  ]);
   const reachableRefs = [...menuItemsSurvey, ...menuItemsQuality].reduce(
     _ => [..._, true],
     createReachableElement(offset)
@@ -193,8 +193,9 @@ const Navigation = ({ className, title }) => {
     }
     if (key === 'down' || key === 'up') {
       const directionFocus = key === 'down' ? NEXT_FOCUS : PREVIOUS_FOCUS;
-      const newRefIndex =
-        getNewFocusElementIndex(directionFocus)(currentFocusElementIndex)(reachableRefs);
+      const newRefIndex = getNewFocusElementIndex(directionFocus)(currentFocusElementIndex)(
+        reachableRefs
+      );
       listRefs[newRefIndex]?.current?.focus();
     }
   };
