d65d1ae0cea61e477f8cf879b8be89fa94496a53
Dylan Decrulle <dylan.decrulle@insee.fr>
2021-12-21 12:48:04 +0100

    update schema with versionning

--- src/utils/indexedbb/idb-config.js
+++ src/utils/indexedbb/idb-config.js
@@ -1,10 +1,12 @@
 import Dexie from 'dexie';
 import schema from './schema.json';
+import schema2 from './schema2.json';
 
 export class LocalDbFactory extends Dexie {
   constructor(dataBaseName) {
     super(dataBaseName);
     this.version(1).stores(schema);
+    this.version(1).stores(schema2);
   }
 
   getStore(name) {
