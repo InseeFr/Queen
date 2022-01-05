1f910337b127b59e7fb8ca06c9a0ac4125eb39ca
laurentC35 <38245508+laurentC35@users.noreply.github.com>
2022-01-05 17:03:48 +0100

    fix clean database (with upgrade)

--- src/utils/indexedbb/services/allTables-idb-service.js
+++ src/utils/indexedbb/services/allTables-idb-service.js
@@ -1,9 +1,11 @@
 import Dexie from 'dexie';
-import schema from '../schema2.json';
+import schema from '../schema.json';
+import schema2 from '../schema2.json';
 
 const clearAllTables = async () => {
   const db = new Dexie('Queen');
-  db.version(2).stores(schema);
+  db.version(1).stores(schema);
+  db.version(2).stores(schema2);
   await db.open();
   await Promise.all(Object.keys(schema).map(table => db.table(table).clear()));
   await db.close();
